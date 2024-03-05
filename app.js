8184cdcdcda2b8b254cce47258977827c9b306a2
Deepanshu Chand <deepanshuchand@Deepanshus-MacBook-Air.local>
2024-03-05 23:48:27 +0530

    Final push 2

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors())
 app.use("/api/v1", mainRouter);
 // app.use('/api/users', userRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 //Seeding data from json file to the database.
 const seedDB = async() => {
